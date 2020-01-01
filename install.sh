81ca182b3d0e4a9d7bc4cc645b1db84ff566f08e
Giacomo <giacomo.tom.petrucci@tiscali.it>
2020-01-01 18:40:54 +0100

    Modified systemd unit and install script to use the included quotes file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 useradd --system --create-home qotd
 cp Motivational_quote_of_the_day_server /usr/bin/
+cp quotes.txt /home/qotd
 cp mqotd.service /etc/systemd/system/
 systemctl start mqotd.service
 systemctl enable mqotd.service
