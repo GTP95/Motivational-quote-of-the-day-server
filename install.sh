297a4229add52bf5bcf57f97a512f27cd9c3ded1
Giacomo <giacomo.tom.petrucci@tiscali.it>
2020-01-06 14:38:00 +0100

    Was unable to find quotes file

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 useradd --system --create-home qotd
 cp build/mqotd /home/qotd
 cp quotes.txt /home/qotd
+chown qotd /home/qotd/mqotd
+chown qotd /home/qotd/quotes.txt
 cp mqotd.service /etc/systemd/system/
 systemctl start mqotd.service
 systemctl enable mqotd.service
