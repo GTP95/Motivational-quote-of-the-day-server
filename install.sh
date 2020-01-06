3918145a2ba94a142a38f4ceeb4896b519730add
Giacomo <giacomo.tom.petrucci@tiscali.it>
2020-01-06 11:58:15 +0100

    Changed name of executable and fixed systemd unit file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 useradd --system --create-home qotd
-cp Motivational_quote_of_the_day_server /usr/bin/
+cp mqotd /usr/bin/
 cp quotes.txt /home/qotd
 cp mqotd.service /etc/systemd/system/
 systemctl start mqotd.service
